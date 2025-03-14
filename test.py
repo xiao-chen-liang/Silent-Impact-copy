324c9029e91754c5980e9af114368879ea9d13df
肖晨亮 <xiao_chen_liang@qq.com>
2025-03-14 09:03:01 +0800

    modify .gitignore and test the project

---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+
+print("hello world")
+# print the python location
+print(sys.executable)
+# print the location of the script
+print(sys.argv[0])
+# print hello world
+print("hello world")
