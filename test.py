3caeb3a9eaa56f42ff6eb7f65d7ddc785404a2cd
肖晨亮 <xiao_chen_liang@qq.com>
2025-03-31 09:00:40 +0800

    prepare data to numpy (31, 401, 6)

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import sys
 
-print("hello world")
-# print the python location
 print(sys.executable)
-# print the location of the script
-print(sys.argv[0])
-# print hello world
-print("hello world")
+print(sys.argv)
+print(sys.path)
